6c85e810e71a84aa18a015eaf516ca12d8ab5005
Allocene <artur.kobylatski@gmail.com>
2024-05-18 16:52:36 +0300

    custom select

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,24 +1,23 @@
-// SearchBar.jsx
 import { useState } from 'react';
 
 import { Button } from '../Button/Button';
 
+import CustomSelect from './CustomSelect';
+
 import styles from './searchbar.module.css';
 
-const SearchBar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [selectedOption, setSelectedOption] = useState('');
+const options = [
+    { value: ' ', label: ' ' },
+    { value: 'price', label: 'Price (High - Low)' },
+    { value: 'newest', label: 'Newest' },
+    { value: 'oldest', label: 'Oldest' },
+];
 
-    const options = [
-        { value: ' ', label: ' ' },
-        { value: 'price', label: 'Price (High - Low)' },
-        { value: 'newest', label: 'Newest' },
-        { value: 'oldest', label: 'Oldest' },
-    ];
+const SearchBar = () => {
+    const [selectedOption, setSelectedOption] = useState('');
 
     const handleSelect = (value: string, label: string) => {
         setSelectedOption(label);
-        setIsOpen(false);
     };
 
     return (
@@ -35,25 +34,7 @@ const SearchBar = () => {
                 </div>
                 <div className={styles.sort}>
                     <p className={styles.text}>Sort by: </p>
-                    <div className={styles.customSelect}>
-                        <div className={styles.selectedOption} onClick={() => setIsOpen(!isOpen)}>
-                            {selectedOption || 'Price (High - Low)'}
-                            <div className={`${styles.arrow} ${isOpen ? styles.open : ''}`}></div>
-                        </div>
-                        {isOpen && (
-                            <div className={styles.options}>
-                                {options.map((option) => (
-                                    <span
-                                        key={option.value}
-                                        className={styles.option}
-                                        onClick={() => handleSelect(option.value, option.label)}
-                                    >
-                                        {option.label}
-                                    </span>
-                                ))}
-                            </div>
-                        )}
-                    </div>
+                    <CustomSelect options={options} selectedOption={selectedOption} onSelect={handleSelect} />
                 </div>
             </div>
         </div>
