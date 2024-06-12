3d7425db20306e3a58bac23896f83c4bc96f8381
Allocene <artur.kobylatski@gmail.com>
2024-06-12 11:50:13 +0300

    task/07-api

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,12 +1,13 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
 
 import { FilterType } from '../../filterType.ts';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner.tsx';
 import { Pagination } from '../Pagination/Pagination';
 import { ProductCard } from '../ProductCard/ProductCard';
 import type Product from '../ProductsInterface/ProductsInterface.tsx';
 import { SearchBar } from '../SearchBar/SearchBar';
-
-import { mock } from './mock';
 
 import styles from './products.module.css';
 
@@ -17,32 +18,50 @@ const filterProducts = (products: Product[], filter: FilterType, searchQuery: st
         .filter((product) => !searchQuery || product.title?.toLowerCase().includes(searchQuery.toLowerCase()))
         .filter((product) => selectedCategories.length === 0 || selectedCategories.includes(product.category?.name ?? ''))
         .sort((a, b) => {
-            switch (filter) {
-                case FilterType.PRICE: {
-                    return (b.price ?? 0) - (a.price ?? 0);
-                }
-                case FilterType.NEWEST: {
-                    return new Date(b.creationAt ?? '').getTime() - new Date(a.creationAt ?? '').getTime();
-                }
-                case FilterType.OLDEST: {
-                    return new Date(a.creationAt ?? '').getTime() - new Date(b.creationAt ?? '').getTime();
-                }
-                default: {
-                    return 0;
-                }
+            if (filter === FilterType.PRICE) {
+                return (b.price ?? 0) - (a.price ?? 0);
             }
+            if (filter === FilterType.NEWEST) {
+                return new Date(b.creationAt ?? '').getTime() - new Date(a.creationAt ?? '').getTime();
+            }
+            if (filter === FilterType.OLDEST) {
+                return new Date(a.creationAt ?? '').getTime() - new Date(b.creationAt ?? '').getTime();
+            }
+            return 0;
         });
 
-export const Products: React.FC<{ isDarkTheme: boolean }> = ({ isDarkTheme }) => {
+export const Products = ({ isDarkTheme }: { isDarkTheme: boolean }) => {
+    const [products, setProducts] = useState<Product[]>([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const [filter, setFilter] = useState<FilterType>(FilterType.PRICE);
+    const [filter, setFilter] = useState(FilterType.PRICE);
     const [searchQuery, setSearchQuery] = useState('');
     const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    const filteredItems = useMemo(
-        () => filterProducts(mock, filter, searchQuery, selectedCategories),
-        [filter, searchQuery, selectedCategories],
-    );
+    useEffect(() => {
+        const fetchProducts = async () => {
+            setIsLoading(true);
+            setError(null);
+            try {
+                const response = await axios.get('https://ma-backend-api.mocintra.com/api/v1/products?limit=50');
+                const productsData = response.data.products;
+                if (Array.isArray(productsData)) {
+                    setProducts(productsData);
+                } else {
+                    throw new TypeError('Invalid data format');
+                }
+            } catch (error_) {
+                console.error('Error fetching products:', error_);
+                setError(null);
+            }
+            setIsLoading(false);
+        };
+
+        fetchProducts();
+    }, []);
+
+    const filteredItems = filterProducts(products, filter, searchQuery, selectedCategories);
 
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
@@ -67,15 +86,31 @@ export const Products: React.FC<{ isDarkTheme: boolean }> = ({ isDarkTheme }) =>
         setCurrentPage(1);
     };
 
+    let content;
+
+    if (isLoading) {
+        content = <LoadingSpinner />;
+    } else if (error) {
+        content = <p>{error}</p>;
+    } else if (filteredItems.length === 0) {
+        content = <LoadingSpinner />; // content = <p className={styles.noProducts}>No products</p>;
+    } else {
+        content = (
+            <>
+                <div className={styles.products}>
+                    {currentItems.map((item) => (
+                        <ProductCard key={item.id ?? ''} product={item} />
+                    ))}
+                </div>
+                <Pagination totalItems={filteredItems.length} itemsPerPage={itemsPerPage} onPageChange={handlePageChange} />
+            </>
+        );
+    }
+
     return (
         <main className={`${styles.main} ${isDarkTheme ? 'dark-theme' : 'light-theme'}`}>
             <SearchBar onFilterChange={handleFilterChange} onSearchChange={handleSearchChange} onCategoryChange={handleCategoryChange} />
-            <div className={styles.products}>
-                {currentItems.map((item) => (
-                    <ProductCard key={item.id ?? ''} product={item} />
-                ))}
-            </div>
-            <Pagination totalItems={filteredItems.length} itemsPerPage={itemsPerPage} onPageChange={handlePageChange} />
+            {content}
         </main>
     );
 };
